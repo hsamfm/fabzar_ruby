768b6d2ce8d04f3a2c5a71525d1cb8a18f6dcbf8
hesam <hsamfm>
2019-07-09 23:51:35 +0430

    deleted file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+p = Product.new(name: "Hidroghen", price: 50)
+
+Cart.all
